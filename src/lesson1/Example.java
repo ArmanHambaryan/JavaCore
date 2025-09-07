8012f7b5ae46ec676ffbb8c8f5cb962cbb46a819
Arman Hambaryan <armanhambaryan@MacBook-Pro-Arman.local>
2025-09-07 15:12:49 +0400

    Initial commit

---
+++ src/lesson1/Example.java
@@ -0,0 +1,9 @@
+package lesson1;
+/*Արման
+Համբարյան
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+    }
+}
---
+++ src/lesson2/Example2.java
@@ -0,0 +1,16 @@
+package lesson2;
+
+public class Example2 {
+
+    public static void main(String[] args) {
+      int num;
+      num = 100;
+        System.out.println("Number = " + num);
+      num = num * 2;
+        System.out.print("Number = ");
+        System.out.println(num);
+
+    }
+
+
+}
