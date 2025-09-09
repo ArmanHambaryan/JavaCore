64f7c542855d1d4e670d0443a9f30697e13be1d3
Arman Hambaryangit config --global user.email armanhambaryan514@gmail.com <armanhambaryan514@gmail.com>
2025-09-09 16:53:05 +0400

    տնային 1

---
+++ src/homework/Homework1.java
@@ -0,0 +1,30 @@
+package homework;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        if (x < y) {
+            System.out.println("y больше x");
+        }
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+
+        }
+        int a = 5;
+        int b = 7;
+        int c = a + b;
+        System.out.println("a + b =  " + c);
+
+       int n = 3;
+        for (int i = 1; i <=10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+
+        }
+
+
+    }
+
+}
+
+
