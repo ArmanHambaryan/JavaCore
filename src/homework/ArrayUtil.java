105ffe5264057f9e501c6438e19c8e32e87e47fe
Arman Hambaryangit config --global user.email armanhambaryan514@gmail.com <armanhambaryan514@gmail.com>
2025-09-16 00:10:04 +0400

    տնային 2 Ուղղված

--- src/homework/ArrayUtil.java
+++ src/homework/ArrayUtil.java
@@ -5,43 +5,55 @@ public class ArrayUtil {
 
         int[] numbers = {1, 6, 3, 9, 15, 52, -3, 5, 8};
         //1
-        System.out.print(numbers[0] + " ");
-        System.out.print(numbers[1] + " ");
-        System.out.print(numbers[2] + " ");
-        System.out.print(numbers[3] + " ");
-        System.out.print(numbers[4] + " ");
-        System.out.print(numbers[5] + " ");
-        System.out.print(numbers[6] + " ");
-        System.out.print(numbers[7] + " ");
-        System.out.println(numbers[8] + " ");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("  " + numbers[i]);
+        }
         System.out.println("-----------------");
         //2
         System.out.println(numbers[0]);
         System.out.println("------------------");
         //3
-        System.out.println(numbers[8]);
+        System.out.println(numbers[numbers.length - 1]);
         System.out.println("------------------");
         //4
         System.out.println(numbers.length);
         System.out.println("------------------");
         //5
-        System.out.println(numbers[6]);
+        int min = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        System.out.println(min);
         System.out.println("------------------");
         //6
         System.out.println(numbers[4]);
         System.out.println("------------------");
         //7
-        System.out.println("Զույգերի քանակը" + " = " + 3);
+        int counter = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                counter++;
+            }
+        }
+        System.out.println(counter);
         System.out.println("------------------");
         //8
-        System.out.println("Կենտերի քանակը" + " = " + 6);
+        int count = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 != 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
         System.out.println("------------------");
         //9
         int sum = 0;
         for (int i = 0; i < numbers.length; i++) {
             sum += numbers[i];
         }
-        System.out.println("Մասիվի էլեմենտների գումարը = " + sum);
+        System.out.println(sum);
         System.out.println("------------------");
         //10
         double average = 0;
@@ -49,7 +61,7 @@ public class ArrayUtil {
             average += numbers[i];
         }
         average /= numbers.length;
-        System.out.println("միջին թվաբանականը = " + average );
+        System.out.println(average);
         System.out.println("------------------");
     }
 }
