f22a0beda45a837d364b0ff2644c83e529fc219b
Arman Hambaryangit config --global user.email armanhambaryan514@gmail.com <armanhambaryan514@gmail.com>
2025-09-22 19:41:30 +0400

    Scanner

---
+++ src/lesson5/ScannerExample.java
@@ -0,0 +1,17 @@
+package lesson5;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("please input name");
+        String name = scanner.nextLine();
+        System.out.println("please input surname");
+        String surname = scanner.nextLine();
+
+        System.out.println("Welcome " + name + " " + surname);
+
+    }
+}
