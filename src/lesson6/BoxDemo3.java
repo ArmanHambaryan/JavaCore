2a6bc8b74e60bb663f724a1451803c563f25a77f
Arman Hambaryangit config --global user.email armanhambaryan514@gmail.com <armanhambaryan514@gmail.com>
2025-09-30 18:36:37 +0400

    lesson 6

---
+++ src/lesson6/BoxDemo3.java
@@ -0,0 +1,26 @@
+package lesson6;
+
+public class BoxDemo3 {
+    public static void main(String[] args) {
+
+        Box mybox1 = new Box();
+        Box mybox2 = new Box();
+
+        double vol;
+
+        mybox1.width = 10;
+        mybox1.height = 20;
+        mybox1.depth = 15;
+
+        mybox2.depth = 3;
+        mybox2.width = 9;
+        mybox2.height = 6;
+
+        vol = mybox1.volume();
+        System.out.println("Oбъeм равен " + vol);
+
+        vol = mybox2.volume();
+        System.out.println("Oбъeм равен " + vol);
+
+    }
+}
