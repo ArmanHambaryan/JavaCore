37ba16cfa75e081b60d174b8c7962f3d6252b9bd
Arman Hambaryangit config --global user.email armanhambaryan514@gmail.com <armanhambaryan514@gmail.com>
2025-10-08 00:23:05 +0400

    տնային

--- src/homework/dynamicarray/DynamicArray.java
+++ src/homework/dynamicarray/DynamicArray.java
@@ -24,18 +24,69 @@ public class DynamicArray {
     }
 
     public int GetByIndex(int index) {
-        if (index >= 0 && index < size){
+        if (index >= 0 && index < size) {
             return array[index];
         }
         return -1;
     }
-    public void print(){
+
+    public void print() {
 
         for (int i = 0; i < size; i++) {
             System.out.println(array[i] + " ");
         }
         System.out.println();
 
+    }
+
+    public void deleteByIndex(int index) {
+
+        if (index < 0 || index >= size)
+            System.out.println("նման ինդեքսով թիվ չկա");
+
+
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        size--;
+    }
+
+    public void set(int index, int value) {
+        if (index < 0 || index >= size) {
+            System.out.println("նման ինդեքսով թիվ չկա");
+        }
+        array[index] = value;
+    }
+
+    public void add(int index, int value) {
+        if (index < 0 || index > size)
+            System.out.println("նման ինդեքսով թիվ չկա");
+
+        if (size == array.length) {
+            extend();
+        }
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = value;
+        size++;
+    }
+
+    public boolean exists(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value)
+                return true;
+        }
+
+        return false;
+    }
+
+    public int getIndexByValue(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value)
+                return i;
+        }
+        return -1;
     }
 
 }
--- src/homework/dynamicarray/DynamicArrayDemo.java
+++ src/homework/dynamicarray/DynamicArrayDemo.java
@@ -12,11 +12,19 @@ public class DynamicArrayDemo {
 
         System.out.println("new elements");
         array.print();
-        System.out.println( " 6 = " +array.GetByIndex(2));
-        System.out.println( " 10 = " +array.GetByIndex(4));
+        System.out.println(" 6 = " + array.GetByIndex(2));
+        System.out.println(" 10 = " + array.GetByIndex(4));
 
-
-
+        array.set(1, 77);
+        array.print();
+        array.add(2, 44);
+        array.print();
+        array.deleteByIndex(3);
+        array.print();
+        System.out.println(" 99 ? " + array.exists(99));
+        System.out.println(" 44 ? " + array.exists(44));
+        System.out.println(" 77 index = " + array.getIndexByValue(77) );
+        System.out.println(" 555 index = " + array.getIndexByValue(555) );
 
     }
 
